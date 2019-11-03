6d35490b57b03edffa79dd22707370fafc00f01c
deimantej <deimantej99@gmail.com>
2019-11-03 21:35:17 +0100

    added footer and header php

---
+++ footer.php
@@ -0,0 +1,14 @@
+<section>
+
+    <footer class="page-footer font-small blue pt-4">
+
+    
+
+      
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3">© 2019 Copyright: Mejls-Orten-Tinghøj Friskole 
+        </div>
+        <!-- Copyright -->
+      
+      </footer>
+    </section>
--- index.php
+++ index.php
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Climate action</title>
-    <link rel="stylesheet" type="text/css" href='<?php echo get_template_directory_uri (); ?> /style.css'>
-    <link rel="stylesheet" type="text/css" href='<?php echo get_template_directory_uri (); ?> /aos.css'>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    
-
-</head>
+<?php get_header(); ?>
 
 <body>
     
@@ -238,20 +223,7 @@
     </div>
 </section>
        <!-----------------------------------footer----------------- -->
-    <section>
-
-    <footer class="page-footer font-small blue pt-4">
-
-    
-
-      
-        <!-- Copyright -->
-        <div class="footer-copyright text-center py-3">© 2019 Copyright: Mejls-Orten-Tinghøj Friskole 
-        </div>
-        <!-- Copyright -->
-      
-      </footer>
-    </section>
+    <?php get_footer (); ?>
 
 
 
