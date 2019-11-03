7a0a6be930eebfc56badea85aac6ea46a16f57a7
Chanele <pandutee69@gmail.com>
2019-11-03 13:24:22 +0100

    origin

---
+++ javascript.js
@@ -0,0 +1,14 @@
+$(document).on('click', 'a[href^="#"]', function (e) {
+    e.preventDefault();
+    $('html, body').stop().animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 800, 'swing');
+});
+
+
+$(function() {
+    $('.pop').on('click', function() {
+        $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+        $('#imagemodal').modal('show');   
+    });		
+});
